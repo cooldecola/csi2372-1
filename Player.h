0c4775e03b59b50f44926bb2964c600b2b2bf31c
Natesh Kukreja <natesh.kukreja@gmail.com>
2017-12-09 22:55:14 -0500

    Add files via upload

--- Player.h
+++ Player.h
@@ -6,15 +6,25 @@ using namespace std;
 class Player {
 public:
 
-
+    //helper variable to check if the user is current or not
     bool isCurrent = false;
+
+    //name of the player
     std::string name="";
+
     //added constructor and getter methods - Natesh
     Player(std::string n):name(n){};
+
+    //methods to be implemented in each individual method
     virtual void inputBeforeRoll(RollOfDice *rollDice)=0;
     virtual void inputAfterRoll(RollOfDice *rollDice)=0;
+
+    //helper getter and setter for the current player
+    //
     bool getIsCurrent(){return isCurrent;};
     void setCurrent(){isCurrent = true;};
+
+    //helper
     std::string getName(){return name;};
 
 };
--- QwintoRow.h
+++ QwintoRow.h
@@ -3,19 +3,28 @@
 //
 
 #ifndef CPP_PROJECT_QWINTOROW_H
+#define TEST_ROW
 #define CPP_PROJECT_QWINTOROW_H
 #include <iostream>
 #include "Colour.h"
 #include "RollOfDice.h"
 
+
 using namespace std;
 
 template <Colour T> class QwintoRow{
 public:
+
+    //overriding operator and returns the
+    //index at the score list
     RollOfDice& operator[] (const int index){
         return scoreList[index];
     };
 
+
+    //create a row of size 9 containing
+    //RollOfDice
+    //New row created for each object that is sent through
     RollOfDice scoreList[9];
 
     ostream& operator<<(ostream &os);
@@ -23,6 +32,16 @@ public:
 
 };
 
+int main(){
 
+    RollOfDice rd;
+    rd.roll();
+    QwintoRow<Colour::RED> red;
+    red[2] = rd;
+
+}
+
+
+#ifndef TEST_ROW
 
 #endif //CPP_PROJECT_QWINTOROW_H
